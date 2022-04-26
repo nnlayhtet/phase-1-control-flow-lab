e1caedd7df4bf5d99241e6d0cecdb200eccd0fb1
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-25 21:08:28 -0400

    commit tree 7c8408550819cb04f35a7a6c6e1c65540b8f198a

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet >= 2500){
+    return 'No can do.'
+  }
+  else if (feet >= 2000 && feet < 2500){
+    return `I will gladly take your thirty bucks.`
+  }
+  else if (feet >= 400 && feet < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return (city === 'NYC') ? ("Ok, sounds good.") : ("No go.");
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break
+    default: return "Bye."
 }
+}
+  
